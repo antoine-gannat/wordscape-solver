1ed11e0af166bf7f919c403245ace8724208b764
antoine <antoine.gannat@gmail.com>
2019-03-09 14:57:52 -0800

    add nodejs solver and online solver

---
+++ nodejs/Parser.js
@@ -0,0 +1,23 @@
+class Parser {
+    parseAvailableLetters(availableLetters) {
+        var availableLettersParsed = availableLetters.toLowerCase();
+        availableLettersParsed = availableLettersParsed.split("");
+
+        var availableLettersWithOccurence = [];
+        availableLettersParsed.forEach((letter) => {
+            const index = availableLettersWithOccurence.findIndex((l) => { return (l.letter === letter); });
+            if (index === -1)
+                availableLettersWithOccurence.push({ letter: letter, occurence: 1 });
+            else
+                availableLettersWithOccurence[index].occurence++;
+        });
+        return (availableLettersWithOccurence);
+    }
+
+    parseInputWord(inputWord) {
+        var inputWordParsed = inputWord.toLowerCase();
+        return (inputWordParsed);
+    }
+}
+
+module.exports = new Parser;
